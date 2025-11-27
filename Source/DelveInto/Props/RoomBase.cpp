e20ebd801de935c510644140f09ddc415cc7c413
9um4 <51898564+9um4@users.noreply.github.com>
2025-11-27 17:36:11 +0900

    feat: RoomBase Actor Class
    `RoomBase`는 Game 내에서 생성될 Room을 추상화한 Layer

---
+++ Source/DelveInto/Props/RoomBase.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Props/RoomBase.h"
+
+ARoomBase::ARoomBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+/**
+ * Room 입장 Trigger가 Trig 되었을 때 호출하는 함수
+ * @param Player `Room`에 입장한 PlayableCharacter
+ */
+void ARoomBase::NotifyPlayerEntered(APlayableCharacter* Player)
+{
+	OnPlayerEnterRoom.Broadcast(Player);
+}
+
+/**
+ * Room 퇴장 Trigger가 Trig 되었을 때 호출하는 함수
+ * @param Player `Room`에서 나간 PlayableCharacter
+ */
+void ARoomBase::NotifyPlayerLeft(APlayableCharacter* Player)
+{
+	OnPlayerLeaveRoom.Broadcast(Player);
+}
+
+
+// Called when the game starts or when spawned
+void ARoomBase::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Display, TEXT("Room이 초기화 되었습니다! %s"), *GetName());
+}
+
+// // Called every frame
+// void ARoomBase::Tick(float DeltaTime)
+// {
+// 	Super::Tick(DeltaTime);
+//
+// }
+
---
+++ Source/DelveInto/Props/RoomBase.h
@@ -0,0 +1,79 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/PlayableCharacter.h"
+#include "GameFramework/Actor.h"
+#include "RoomBase.generated.h"
+
+/**
+ * 방의 연결 상태를 나타내는 구조체
+ */
+USTRUCT(BlueprintType)
+struct FRoomConnectionInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, BluePrintReadOnly)
+	bool bIsConnectedToNorth;
+	
+	UPROPERTY(EditDefaultsOnly, BluePrintReadOnly)
+	bool bIsConnectedToSouth;
+	
+	UPROPERTY(EditDefaultsOnly, BluePrintReadOnly)
+	bool bIsConnectedToEast;
+
+	UPROPERTY(EditDefaultsOnly, BluePrintReadOnly)
+	bool bIsConnectedToWest;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	bool bIsConnectedToUp;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	bool bIsConnectedToDown;
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnPlayerEnterRoom, AActor*, Player);
+
+/**
+ * 방의 기본 Actor 클래스
+ */
+UCLASS()
+class DELVEINTO_API ARoomBase : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Ticking을 사용하지 않는 것을 명시적으로 지정
+	ARoomBase();
+
+	// 방의 연결 상태 정보
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Room|Info")
+	FRoomConnectionInfo RoomConnectionInfo = FRoomConnectionInfo();
+
+	// Subscriber들이 Binding할 Player 입장 Event
+	UPROPERTY(BlueprintAssignable, Category="Room|Event")
+	FOnPlayerEnterRoom OnPlayerEnterRoom;
+
+	// Subscriber들이 Binding할 Player 퇴장 Event
+	UPROPERTY(BlueprintAssignable, Category="Room|Event")
+	FOnPlayerEnterRoom OnPlayerLeaveRoom;
+
+	// Room 입장 Trigger가 Trig 되었을 때 호출하는 함수
+	UFUNCTION(BlueprintCallable, Category="Room|Event")
+	void NotifyPlayerEntered(APlayableCharacter* Player);
+
+	// Room 퇴장 Trigger가 Trig 되었을 때 호출하는 함수
+	UFUNCTION(BlueprintCallable, Category="Room|Event")
+	void NotifyPlayerLeft(APlayableCharacter* Player);
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+// public:	
+// 	// Called every frame
+// 	virtual void Tick(float DeltaTime) override;
+
+};
